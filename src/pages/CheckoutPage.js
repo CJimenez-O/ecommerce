9be2683c7c504449b7e6f07cea2394e824152b75
legazy33 <40193720+legazy33@users.noreply.github.com>
2021-05-25 14:58:09 -0700

    created a private route to prevent a user from checking out without loggin in

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -6,12 +6,21 @@ import { useCartContext } from "../context/cart_context";
 import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
-	// const { cart } = useCartContext();
+	const { cart } = useCartContext();
 	return (
 		<main>
 			<PageHero title="checkout" />
 			<Wrapper className="page">
-				<h3>Hello</h3>
+				{cart.length < 1 ? (
+					<div className="empty">
+						<h2>your cart is empty</h2>
+						<Link to="/products" className="btn">
+							fill it
+						</Link>
+					</div>
+				) : (
+					<StripeCheckout />
+				)}
 			</Wrapper>
 		</main>
 	);
